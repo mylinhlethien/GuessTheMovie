2463f3b155a7f6d00436afdbf958f64431d4db16
My-Linh Le Thien <mylinh.lethien@gmail.com>
2021-11-17 11:52:12 +0100

    design 2 activities

---
+++ app/src/main/java/fr/isep/guessthemovie/LaunchActivity.java
@@ -0,0 +1,14 @@
+package fr.isep.guessthemovie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
