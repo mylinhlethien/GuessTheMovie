32f3d6df3b5f1aff7f78e5e2b02dc5820f2649c1
My-Linh Le Thien <mylinh.lethien@gmail.com>
2021-11-30 11:47:25 +0100

    display random movie details

--- app/src/main/java/fr/isep/guessthemovie/LevelOneActivity.java
+++ app/src/main/java/fr/isep/guessthemovie/LevelOneActivity.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import java.util.Random;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -40,25 +42,39 @@ public class LevelOneActivity extends AppCompatActivity {
         MovieInterface movieInterface = MovieClass.getMovieInstance().create(MovieInterface.class);
         MovieInterface pictureInterface = PictureClass.getPictureInstance().create(MovieInterface.class);
 
-        getPopularMovies(movieInterface);
-        movieDetailsLevelOne(movieInterface);
-
-        movieActors(movieInterface, pictureInterface);
-        movieActors(movieInterface,pictureInterface);
+        getPopularMovies(movieInterface, pictureInterface);
     }
 
     
-    private void getPopularMovies(MovieInterface movieInterface) {
-        Call<JsonObject> call = movieInterface.getPopularMovies("1");
+    private void getPopularMovies(MovieInterface movieInterface, MovieInterface pictureInterface) {
+        // Generate random page number between 1-20
+        Random r = new Random();
+        int low = 1; //included
+        int high = 21; //not included
+        int randomPageNumber = r.nextInt(high-low) + low;
+
+        // Generate movieId between 0-19
+        Random r1 = new Random();
+        int low1 = 0; //included
+        int high1 = 20; //not included
+        int randomMovieId = r1.nextInt(high1-low1) + low1;
+
+        Call<JsonObject> call = movieInterface.getPopularMovies(randomPageNumber);
         Log.d("call popular movies", String.valueOf(call.request().url()));
         call.enqueue(new Callback<JsonObject>() {
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                 JsonObject res = response.body();
                 Log.d("Popular movie", String.valueOf(res));
-                JsonElement cast = res.get("results");
-                JsonElement popularMovieId = cast.getAsJsonArray().get(1).getAsJsonObject().get("id");
+                JsonElement results = res.get("results");
+                JsonElement popularMovieId = results.getAsJsonArray().get(randomMovieId).getAsJsonObject().get("id");
                 Log.d("Popular movie Id", String.valueOf(popularMovieId));
+
+
+                //call the other methods
+                movieDetailsLevelOne(movieInterface, Integer.parseInt(String.valueOf(popularMovieId)));
+                movieActors(movieInterface, pictureInterface, Integer.parseInt(String.valueOf(popularMovieId)));
+                movieActors(movieInterface,pictureInterface, Integer.parseInt(String.valueOf(popularMovieId)));
             }
 
             @Override
@@ -69,29 +85,29 @@ public class LevelOneActivity extends AppCompatActivity {
         });
     }
 
-    private void movieActors(MovieInterface movieInterface, MovieInterface pictureInterface) {
-        Call<JsonObject> call = movieInterface.getMovieActors("299534");
+    private void movieActors(MovieInterface movieInterface, MovieInterface pictureInterface, int movieId) {
+        Call<JsonObject> call = movieInterface.getMovieActors(movieId);
         Log.d("call movie actors", String.valueOf(call.request().url()));
         call.enqueue(new Callback<JsonObject>() {
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                 JsonObject res = response.body();
-                Log.d("res", String.valueOf(res));
+                //Log.d("res", String.valueOf(res));
                 JsonElement cast = res.get("cast");
-                Log.d("cast", String.valueOf(cast));
+                //Log.d("cast", String.valueOf(cast));
                 JsonElement actorName1 = cast.getAsJsonArray().get(0).getAsJsonObject().get("name");
                 JsonElement characterName1 = cast.getAsJsonArray().get(0).getAsJsonObject().get("character");
                 JsonElement actorPicture1 = cast.getAsJsonArray().get(0).getAsJsonObject().get("profile_path");
-                Log.d("actorName 0", String.valueOf(actorName1));
-                Log.d("actorCharacter 0", String.valueOf(characterName1));
+                //Log.d("actorName 0", String.valueOf(actorName1));
+                //Log.d("actorCharacter 0", String.valueOf(characterName1));
                 Log.d("actorPicture 0 ", String.valueOf(actorPicture1));
 
                 JsonElement actorName2 = cast.getAsJsonArray().get(1).getAsJsonObject().get("name");
                 JsonElement characterName2 = cast.getAsJsonArray().get(1).getAsJsonObject().get("character");
                 JsonElement actorPicture2 = cast.getAsJsonArray().get(1).getAsJsonObject().get("profile_path");
-                Log.d("actorName 0", String.valueOf(actorName2));
-                Log.d("actorCharacter 0", String.valueOf(characterName2));
-                Log.d("actorPicture 0 ", String.valueOf(actorPicture2));
+               // Log.d("actorName 1", String.valueOf(actorName2));
+                //Log.d("actorCharacter 1", String.valueOf(characterName2));
+                Log.d("actorPicture 1 ", String.valueOf(actorPicture2));
 
                 actor1Txt.setText(String.valueOf(actorName1));
                 actor2Txt.setText(String.valueOf(actorName2));
@@ -108,20 +124,20 @@ public class LevelOneActivity extends AppCompatActivity {
         });
     }
 
-    private void movieDetailsLevelOne(MovieInterface movieInterface) {
-        Call<JsonObject> call = movieInterface.getMovieDetails("299534");
+    private void movieDetailsLevelOne(MovieInterface movieInterface, int movieId) {
+        Call<JsonObject> call = movieInterface.getMovieDetails(movieId);
         Log.d("call movie details level 1", String.valueOf(call.request().url()));
         call.enqueue(new Callback<JsonObject>() {
             @Override
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                 JsonObject res = response.body();
-                Log.d("res", String.valueOf(res));
+                //Log.d("res", String.valueOf(res));
                 JsonElement originalTitle = res.get("original_title");
                 JsonElement overview = res.get("overview");
                 JsonElement releaseDate = res.get("release_date");
                 Log.d("title", String.valueOf(originalTitle));
-                Log.d("overview", String.valueOf(overview));
-                Log.d("releaseDate", String.valueOf(releaseDate));
+                //Log.d("overview", String.valueOf(overview));
+                //Log.d("releaseDate", String.valueOf(releaseDate));
 
                 releaseDateTxt.setText(String.valueOf(releaseDate));
                 overviewTxt.setText(String.valueOf(overview));
--- app/src/main/java/retrofitPackage/MovieInterface.java
+++ app/src/main/java/retrofitPackage/MovieInterface.java
@@ -9,14 +9,14 @@ import retrofit2.http.Query;
 
 public interface MovieInterface {
     @GET("movie/{movieId}?api_key=64a67a88869b332a993437d0b110879b&language=en-US")
-    Call<JsonObject> getMovieDetails(@Path("movieId") String movieId);
+    Call<JsonObject> getMovieDetails(@Path("movieId") int movieId);
 
     @GET("movie/{movieId}/credits?api_key=64a67a88869b332a993437d0b110879b&language=en-US")
-    Call<JsonObject> getMovieActors(@Path("movieId") String movieId);
+    Call<JsonObject> getMovieActors(@Path("movieId") int movieId);
 
     @GET("{picture_path}")
     Call<JsonObject> getMovieActorPicture(@Path("picture_path") String picture_path);
 
     @GET("movie/popular?api_key=64a67a88869b332a993437d0b110879b&language=en-US")
-    Call<JsonObject> getPopularMovies(@Query("page") String page_number);
+    Call<JsonObject> getPopularMovies(@Query("page") int page_number);
 }
