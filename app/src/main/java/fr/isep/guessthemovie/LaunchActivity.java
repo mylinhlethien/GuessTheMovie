639b0cbdc37776f48396dfd270be03de0e067633
SalmaMrassi <salmamrassi@yahoo.fr>
2021-12-23 22:37:00 +0100

    set image dans launch activity

--- app/src/main/java/fr/isep/guessthemovie/LaunchActivity.java
+++ app/src/main/java/fr/isep/guessthemovie/LaunchActivity.java
@@ -6,17 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
 
 public class LaunchActivity extends AppCompatActivity {
 
     Spinner levelDifficulty;
+    ImageView imageView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
 
+        imageView = findViewById(R.id.imageView);
         levelDifficulty = findViewById(R.id.levelDifficulty);
         String[] dropdownList = {"Beginner", "Intermediate", "Expert"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, dropdownList);
